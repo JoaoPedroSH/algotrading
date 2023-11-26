5a9a94bf72f58c26e31fee090d6c2f6358f878f3
JoaoPedroSH <84398388+JoaoPedroSH@users.noreply.github.com>
2023-11-26 15:41:41 -0300

    Update mt5.py

--- src/app/services/mt5/mt5.py
+++ src/app/services/mt5/mt5.py
@@ -94,16 +94,10 @@ def orderOpen():
     import time
     import MetaTrader5 as mt5
 
-    # display data on the MetaTrader 5 package
-    print("MetaTrader5 package author: ", mt5.__author__)
-    print("MetaTrader5 package version: ", mt5.__version__)
-
-    # establish connection to the MetaTrader 5 terminal
     if not mt5.initialize():
         print("initialize() failed, error code =", mt5.last_error())
         quit()
 
-    # prepare the buy request structure
     symbol = "WINZ23"
     symbol_info = mt5.symbol_info(symbol)
     if symbol_info is None:
@@ -111,7 +105,6 @@ def orderOpen():
         mt5.shutdown()
         quit()
 
-    # if the symbol is unavailable in MarketWatch, add it
     if not symbol_info.visible:
         print(symbol, "is not visible, trying to switch on")
         if not mt5.symbol_select(symbol, True):
@@ -138,9 +131,8 @@ def orderOpen():
         "type_filling": mt5.ORDER_FILLING_RETURN,
     }
 
-    # send a trading request
+
     result = mt5.order_send(request)
-    # check the execution result
     print(
         "1. order_send(): by {} {} lots at {} with deviation={} points".format(
             symbol, lot, price, deviation
@@ -148,11 +140,9 @@ def orderOpen():
     )
     if result.retcode != mt5.TRADE_RETCODE_DONE:
         print("2. order_send failed, retcode={}".format(result.retcode))
-        # request the result as a dictionary and display it element by element
         result_dict = result._asdict()
         for field in result_dict.keys():
             print("   {}={}".format(field, result_dict[field]))
-            # if this is a trading request structure, display it element by element as well
             if field == "request":
                 traderequest_dict = result_dict[field]._asdict()
                 for tradereq_filed in traderequest_dict:
@@ -171,11 +161,7 @@ def orderOpen():
     time.sleep(2)
 
 
-orderOpen()
-
-
 def orderClose():
-    # create a close request
     symbol = "WINZ23"
     lot = 0.1
     position_id = result.order
@@ -194,9 +180,7 @@ def orderClose():
         "type_time": mt5.ORDER_TIME_GTC,
         "type_filling": mt5.ORDER_FILLING_RETURN,
     }
-    # send a trading request
     result = mt5.order_send(request)
-    # check the execution result
     print(
         "3. close position #{}: sell {} {} lots at {} with deviation={} points".format(
             position_id, symbol, lot, price, deviation
@@ -207,11 +191,9 @@ def orderClose():
         print("   result", result)
     else:
         print("4. position #{} closed, {}".format(position_id, result))
-        # request the result as a dictionary and display it element by element
         result_dict = result._asdict()
         for field in result_dict.keys():
             print("   {}={}".format(field, result_dict[field]))
-            # if this is a trading request structure, display it element by element as well
             if field == "request":
                 traderequest_dict = result_dict[field]._asdict()
                 for tradereq_filed in traderequest_dict:
@@ -220,5 +202,4 @@ def orderClose():
                             tradereq_filed, traderequest_dict[tradereq_filed]
                         )
                     )
-    # shut down connection to the MetaTrader 5 terminal
     mt5.shutdown()
